7f60ed278934b554816eecfcea0f7399ca7aeea7
jk9642 <jagadeesh@vulcanflow.com>
2025-01-07 09:46:42 +0530

    changed the name of the file

--- Try/app/Http/Controllers/Controller.php
+++ Try/app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Get views
+    public function index()
+    {
+        return view('index'); 
+    }
 }
--- Try/routes/web.php
+++ Try/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+Route::get('/index', [Controller::class, 'index'])->name('index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
